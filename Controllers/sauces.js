c227398266da58a3e2617f088315c4ea155bb6df
Benjamin <bpretet59@gmail.com>
2022-05-24 11:12:11 +0200

    mise a jour User et création partie sauce

---
+++ Controllers/sauces.js
@@ -0,0 +1,51 @@
+const Thing = require('../models/sauces')
+const fs = require('fs');
+const sauces = require('../models/sauces');
+
+exports.createSauce = (req, res, next) => {
+  const saucesObject = JSON.parse(req.body.thing);
+  delete saucesObject._id;
+  const sauces = new Sauces({
+    ...saucesObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  });
+  sauces.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.modifySauces = (req, res, next) => {
+  const saucesObject = req.file ?
+    {
+      ...JSON.parse(req.body.sauces),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  sauces.updateOne({ _id: req.params.id }, { ...saucesObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.deleteSauces = (req, res, next) => {
+  sauces.findOne({ _id: req.params.id })
+    .then(sauces => {
+      const filename = sauces.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Thing.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+          .catch(error => res.status(400).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+exports.getOneSauces = (req, res, next) => {
+    sauces.findOne({ _id: req.params.id })
+      .then(sauces => res.status(200).json(sauces))
+      .catch(error => res.status(404).json({ error }));
+};
+
+exports.getAllSauces = (req, res, next) => {
+    sauces.find()
+      .then(sauces => res.status(200).json(sauces))
+      .catch(error => res.status(400).json({ error }));
+};
---
+++ Routes/sauces.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+
+const stuffCtrl = require('../controllers/sauces');
+
+router.post('/', auth, multer, saucesCtrl.createSauces);
+router.put('/:id', auth, multer, saucesCtrl.modifySauces);
+router.delete('/:id', auth, saucesCtrl.deleteSauces);
+router.get('/:id', auth, saucesCtrl.getOneSauces);
+router.get('/', auth, saucesCtrl.getAllSauces);
+
+module.exports = router;
